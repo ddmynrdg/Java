464bde22cb8148d6a199c0dfa304026a4ee5cb51
Didem Yanardağ <didem.yanardag@bil.omu.edu.tr>
2015-11-08 09:34:06 +0200

    Update joptionPane.java

--- Deneme/src/pane/com/joptionPane.java
+++ Deneme/src/pane/com/joptionPane.java
@@ -11,17 +11,14 @@ public class joptionPane {
 			vize = JOptionPane.showInputDialog(null, "Vize notunuzu tekrar girin: ");
 			sayi1 = Integer.parseInt(vize);
 		}
-		
 		String fnal = JOptionPane.showInputDialog(null, "Final notunuzu girin: ");
 		int sayi2 = Integer.parseInt(fnal);
 		while (sayi2 <0 || sayi2 >100) {
 			fnal = JOptionPane.showInputDialog(null, "Final notunuzu tekrar girin: ");
 			sayi2 = Integer.parseInt(fnal);
 		}
-	    int ort = (int) (sayi1 * 0.4 + sayi2 * 0.6);
-	    String sonuc = "Notunuz : " + ort ;
-	    JOptionPane.showMessageDialog(null, sonuc, "Not acýklama", JOptionPane.INFORMATION_MESSAGE );
-		
+	    	int ort = (int) (sayi1 * 0.4 + sayi2 * 0.6);
+	    	String sonuc = "Notunuz : " + ort ;
+	    	JOptionPane.showMessageDialog(null, sonuc, "Not aciklama", JOptionPane.INFORMATION_MESSAGE );
 	}
-
 }
