844ac207d982bafea193c46706b1977eb3d03005
Didem Yanardağ <didem.yanardag@bil.omu.edu.tr>
2015-11-08 09:35:26 +0200

    Update JOption1.java

--- Deneme/src/pane/com/JOption1.java
+++ Deneme/src/pane/com/JOption1.java
@@ -4,13 +4,10 @@ import javax.swing.JOptionPane;
 
 public class JOption1 {
 
-
 	public static void main(String[] args) {
-		String girilen = JOptionPane.showInputDialog("bir ddeger gir:");
+		String girilen = JOptionPane.showInputDialog("Bir deger girin:");
 		int sayi = Integer.parseInt(girilen);
 		sayi *= 5;
 		JOptionPane.showMessageDialog(null, sayi, "sonuc", JOptionPane.ERROR_MESSAGE);
-
 	}
-
 }
